d0f3b0a92c350751d679045f922d9f1722604a09
David-Salihu <davidsalihu19@gmail.com>
2024-09-22 18:31:46 +0100

    Compute total bill

--- client/src/screens/CartPage.tsx
+++ client/src/screens/CartPage.tsx
@@ -1,4 +1,4 @@
-import { FormEvent } from "react"
+import { FormEvent, useEffect, useState } from "react"
 import { CartProductCard } from "../components/CartProductCard"
 import "./CartPage.css"
 
@@ -54,13 +54,46 @@ const products = [
         id: '52334'
     }
 ]
-
+type ItemChecked = {
+    id: string 
+    checked: boolean
+    price: number
+}
 export function CartPage() {
+    const [itemStatus, setItemStatus] = useState<ItemChecked[]>([])
+    const [totalBill, setTotalBill] = useState(0);
+
+    useEffect(()=> {
+        let states:ItemChecked[] = [];
+        products.forEach(item => {
+            states.push({id: item.id, checked: false, price: item.price});
+        })
+        setItemStatus(states);
+    }, []);
 
     function handleCheckOut(e: FormEvent) {
         e.preventDefault();
-        
+
         console.log("submitted")
+        console.log(itemStatus)
+    }
+
+    function handleChange(id: string, state: boolean){
+        setItemStatus(preValue => {
+            return preValue.map(item => {
+                if(item.id == id && item.checked == true && state == false) { // if item was checked before but is now unchecked, substract it's price from the total bill
+                    setTotalBill((preVal) => {
+                        return preVal - item.price
+                    }); 
+                }else if(item.id == id && item.checked == false && state == true) { // if item was not checked before but is now checked, add it's price from the total bill
+                    setTotalBill((preVal) => {
+                        return preVal + item.price
+                    });
+                }
+                console.log("here")
+               return (item.id == id)? { ...item, checked: state } : item
+            })
+        });
     }
 
     return (
@@ -69,10 +102,10 @@ export function CartPage() {
                 <h1>Shopping Cart ({sessionStorage.getItem("cartCount")})</h1>
                 <form id="add-product-form" onSubmit={handleCheckOut}>
                     <div id="cart-list">
-                        {products.map((product, index) => {
+                        {products.map((product) => {
                             return (
                                 <div className="cart-item" key={`${product.id}`}>
-                                    <input type="checkbox" className="check-box" name="vehicle2" value="Car"></input>
+                                    <input type="checkbox" className="check-box" name="vehicle2" onChange={ (e) => handleChange(product.id, e.target.checked)}></input>
                                     <label htmlFor="vehicle2"> 
                                         <CartProductCard image={product.image} name={product.name} price={product.price} qty={3} id={product.id}/>
                                     </label>
@@ -86,15 +119,15 @@ export function CartPage() {
                 <h1>Summary</h1>
                 <div className="summary-item">
                     <p>Subtotal</p>
-                    <p>₦ 12,745.39</p>
+                    <p>₦ {totalBill}</p>
                 </div>
                 <div className="summary-item">
                     <p>Shipping fee</p>
-                    <p>₦ 7,215.72</p>
+                    <p>₦ 1500</p>
                 </div>
                 <div className="summary-item">
                     <p style={{fontWeight: "bold", lineHeight: "20px"}}>Total</p>
-                    <p style={{fontSize: "20px", fontWeight: "bold"}}>₦ 19,507.86</p>
+                    <p style={{fontSize: "20px", fontWeight: "bold", color: "black"}}>₦ {totalBill + 1500}</p>
                 </div>
                 <button form="add-product-form" className="checkout-button" type="submit" style={{width: "100%"}}>Checkout</button>
             </div>
